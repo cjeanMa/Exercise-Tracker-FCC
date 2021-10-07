9d935c05ea2151f8d528a2c2b600040b1c08a3fb
JmCastle <cjeanma009@gmail.com>
2021-10-07 12:28:25 -0500

    fixing littles details to pass the challenge

--- config/config.js
+++ config/config.js
@@ -2,20 +2,20 @@
 require('dotenv').config();
 module.exports = {
     app: {
-        name: process.env.APP_NAME || "4th problem fcc test",
-        port: process.env.APP_PORT || 5000,
-        enviroment: process.env.APP_ENV || "dev",
+        name: process.env.APP_NAME,
+        port: process.env.APP_PORT,
+        enviroment: process.env.APP_ENV,
         logpath: process.env.LOG_PATH
     },
     mongo: {
-        host: process.env.MONGODB_HOST || "clustermongo.rx8jg.mongodb.net",
-        database: process.env.MONGODB_NAME || "myFirstDatabase",
-        user: process.env.MONGODB_USER || "mongoUser",
-        password: process.env.MONGODB_PASSWORD || "admin123"
+        host: process.env.MONGODB_HOST,
+        database: process.env.MONGODB_NAME,
+        user: process.env.MONGODB_USER,
+        password: process.env.MONGODB_PASSWORD,
     },
     loggin: {
         file: process.env.LOG_PATH,
-        level: process.env.LOG_LEVEL || 'info',
-        console: process.env.LOG_ENABLE_CONSOLE || true
+        level: process.env.LOG_LEVEL,
+        console: process.env.LOG_ENABLE_CONSOLE,
       }
 }
